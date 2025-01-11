f6060c0192d56e5406caa08ac54ad18d25ea8ed5
kalchaqui <diego.rbarrionuevo@gmail.com>
2025-01-11 16:34:35 -0300

    quinto commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+import MenuItem from "./components/Menuitem"
 import { menuItems } from "./data/db"
 
 function App() {
 
   return (
     <>
-    <header className="bg-teal-400 py-5">
-      <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
+    <header className="bg-black py-5">
+      <h1 className="text-white text-center text-3xl font-mono ">Calculadora de Propinas y Consumo</h1>
     </header>
 
     <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
     <div>
     <h2>Menú</h2>
+
+    {menuItems.map(item =>(
+      <MenuItem
+      key={item.id}
+      item={item}
+      />
+    ))}
     </div>
 
     <div>
